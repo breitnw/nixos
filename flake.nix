2bf3b52029150efd0c408c4bc6b70b487a07aefc
Nick Breitling <breitling.nw@gmail.com>
2024-09-04 17:01:36 -0500

    initial structure

--- flake.nix
+++ flake.nix
@@ -1,15 +1,41 @@
 {
-  description = "A very basic flake";
+  description = "breitnw's nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    system = "aarch64-linux";
+  in {
+    formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
 
-    packages.aarch64-linux.hello = nixpkgs.legacyPackages.aarch64-linux.hello;
+    nixosConfigurations = {
+      nix = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # hardware-configuration.nix is imported by configuration.nix
+          ./nixos/configuration.nix
+        ];
+      };
+    };
 
-    packages.aarch64-linux.default = self.packages.aarch64-linux.hello;
-
+    homeConfigurations = {
+      "breitnw@nix" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          ./home-manager/home.nix
+        ];
+      };
+    };
   };
 }
---
+++ home-manager/home.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # ...
+  ];
+
+  nixpkgs = {
+    overlays = [
+
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "breitnw";
+    homeDirectory = "/home/breitnw";
+  };
+
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "24.11";
+}
