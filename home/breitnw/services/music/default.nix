c65a71dc4a231d3b3c020f5f1353f71beeabd9ed
Nick Breitling <breitling.nw@gmail.com>
2025-08-02 21:22:33 -0500

    Favorite sticker for RMPC

--- home/breitnw/services/music/default.nix
+++ home/breitnw/services/music/default.nix
@@ -1,9 +1,4 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
+{pkgs, ...}:
 # TODO use a program like mpdpopm or mpdfav for favorites
 {
   services.mpd = {
@@ -24,8 +19,29 @@
     pkgs.unstable.rmpc
   ];
   # the rmpc module in home-manager doesn't have the themes directory :(
-  xdg.configFile.rmpc = {
-    source = ./rmpc;
-    recursive = true;
+  xdg.configFile = let
+    rmpc-bin = "${pkgs.rmpc}/bin/rmpc";
+    jq-bin = "${pkgs.jq}/bin/jq";
+    rmpc-toggle-favorite = pkgs.writeShellScriptBin "rmpc-toggle-favorite" ''
+      IFS=$'\n' # make newlines the only separator
+      for SONG in $SELECTED_SONGS; do
+        sticker=$(${rmpc-bin} sticker get "$SONG" "favorited" | ${jq-bin} -r '.value')
+
+        if [ -z "$sticker" ]; then
+          ${rmpc-bin} sticker set "$SONG" "favorited" "★"
+        else
+          ${rmpc-bin} sticker delete "$SONG" "favorited"
+        fi
+      done
+    '';
+    rmpc-toggle-favorite-bin = "${rmpc-toggle-favorite}/bin/rmpc-toggle-favorite";
+  in {
+    "rmpc/config.ron".source = pkgs.replaceVars ./rmpc/config.ron {
+      rmpc-toggle-favorite = rmpc-toggle-favorite-bin;
+    };
+    "rmpc/themes" = {
+      source = ./rmpc/themes;
+      recursive = true;
+    };
   };
 }
