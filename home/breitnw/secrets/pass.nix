c315b37b486fc5b40753e0b9d88849ccbbc10e1d
Nick Breitling <breitling.nw@gmail.com>
2025-12-26 17:50:33 -0600

    ZSH integration for GPG (may or may not be necessary)

--- home/breitnw/secrets/pass.nix
+++ home/breitnw/secrets/pass.nix
@@ -5,6 +5,7 @@
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
+    enableZshIntegration = true;
     pinentry.package = pkgs.pinentry-qt;
   };
   # gui for viewing passwords
